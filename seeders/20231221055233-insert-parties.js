dca8eed4ca5724a8ee9b165c7f598ca78e964da9
alishaputri5 <alishaputri5@gmail.com>
2023-12-21 13:19:27 +0700

    insert seed

---
+++ seeders/20231221055233-insert-parties.js
@@ -0,0 +1,33 @@
+'use strict';
+const fs = require('fs').promises
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    let data = JSON.parse(await fs.readFile('./parties.json', 'utf-8'))
+    console.log(data);
+    let result = data.map(el => {
+      el.createdAt = el.updatedAt = new Date()
+      return el
+    })
+    await queryInterface.bulkInsert("Parties", result)
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete("Parties", null)
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
